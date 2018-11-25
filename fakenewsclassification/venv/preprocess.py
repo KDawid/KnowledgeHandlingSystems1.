Fix word2Vec preprocessing with TF-IDF normalisation

fakenewsclassification/venv/preprocess.py
@@ -14,7 +14,7 @@ from pprint import pprint  # pretty-printer
 from unidecode import unidecode
 
 CONFIG_FILE_PATH = "config.json"
-#spacy_nlp = spacy.load('en_core_web_sm')
+spacy_nlp = spacy.load('en_core_web_sm')
 
 # more preprocess options: https://radimrehurek.com/gensim/parsing/preprocessing.html
 STOPLIST = set('for a of the and to in'.split())
@@ -38,6 +38,7 @@ class FakeNewsPreprocesser:
         self.RESULT_FILE_PATH = config["RESULT_FILE_PATH"]
         self.TF_IDF_VECTOR_FILE_PATH = config["TF_IDF_VECTOR_FILE_PATH"]
         self.WORD2VEC_VECTOR_FILE_PATH = config["WORD2VEC_VECTOR_FILE_PATH"]
+        self.WORD2VEC_TFIDF_VECTOR_FILE_PATH = config["WORD2VEC_TFIDF_VECTOR_FILE_PATH"]
         self.WORD2VEC_MODEL_FILE_PATH = config["WORD2VEC_MODEL_FILE_PATH"]
 
     def readJson(self):
@@ -200,10 +201,11 @@ class FakeNewsPreprocesser:
     # source: http://nadbordrozd.github.io/blog/2016/05/20/text-classification-with-word2vec/
     def word2VecTransformation(self, texts, data):
         model = KeyedVectors.load_word2vec_format(self.WORD2VEC_MODEL_FILE_PATH, binary=True)
-        w2v = dict(zip(model.wv.index2word, model.wv.syn0))
+        word2vec = dict(zip(model.wv.index2word, model.wv.syn0))
+        dim = len(next(iter(word2vec.items())))
         result = np.array([
             np.mean([model[w] for w in words if w in model]
-                    or [np.zeros(self.dim)], axis=0)
+                    or [np.zeros(dim)], axis=0)
             for words in texts
         ])
         df = pd.DataFrame(result, index=range(len(result)))
@@ -217,15 +219,16 @@ class FakeNewsPreprocesser:
         model = KeyedVectors.load_word2vec_format(self.WORD2VEC_MODEL_FILE_PATH, binary=True)
         w2v = dict(zip(model.wv.index2word, model.wv.syn0))
 
-        dim = len(w2v.items().__next__())
+        dim = len(next(iter(w2v.items())))
 
         from sklearn.feature_extraction.text import TfidfVectorizer
-        tfidf = TfidfVectorizer(analyzer=lambda texts: texts)
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
         tfidf.fit(texts)
 
         from collections import defaultdict
 
         max_idf = max(tfidf.idf_)
+        print(tfidf.vocabulary_.items())
         word2weight = defaultdict(
             lambda: max_idf, [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
 
@@ -238,7 +241,7 @@ class FakeNewsPreprocesser:
         df = pd.DataFrame(result, index=range(len(result)))
         df['type'] = data['type']
         result = df.to_dict()
-        with open(self.WORD2VEC_VECTOR_FILE_PATH, 'w') as f:
+        with open(self.WORD2VEC_TFIDF_VECTOR_FILE_PATH, 'w') as f:
             out = json.dumps(result, indent=4)
             f.write(out)
 
@@ -246,8 +249,8 @@ preprocesser = FakeNewsPreprocesser(CONFIG_FILE_PATH)
 
 jsonData = preprocesser.readJson()
 
-texts = preprocesser.preprocessText(jsonData, PREPROCESS_TYPE.GENSIM)
-#texts = preprocesser.callSpacy(jsonData)
+#texts = preprocesser.preprocessText(jsonData, PREPROCESS_TYPE.GENSIM)
+texts = preprocesser.callSpacy(jsonData)
 
 #preprocesser.tfIdfTransformation(texts, jsonData)
 #preprocesser.word2VecTransformation(texts, jsonData)